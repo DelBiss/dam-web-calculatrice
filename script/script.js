b285d8e858c340a72fc4ac07b698817833d752b2
Philippe Allard-Rousse <philrousse@gmail.com>
2021-12-07 09:16:14 -0500

    Api Query implemented

--- script/script.js
+++ script/script.js
@@ -39,3 +39,43 @@ function Refresh() {
 window.Input = doInput
 window.Clear = Clear
 window.Delete = Delete
+
+function TestMeteo() {
+    let requestURL = 'http://api.weatherapi.com/v1/current.json?key=f111c71466e14bca870155327210212%20&q=Montreal&aqi=no&lang=fr';
+    let request = new XMLHttpRequest();
+    request.open('GET', requestURL);
+    request.responseType = 'json';
+    request.send();
+
+    request.onload = function() {
+        const superHeroes = request.response;
+        // populateHeader(superHeroes);
+        PopulateMeteo(superHeroes);
+    }
+
+}
+
+var section = document.getElementById('meteo');
+
+function PopulateMeteo(jsonObj) {
+
+
+    var myArticle = document.createElement('article');
+    var myH2 = document.createElement('p');
+    var myPara1 = document.createElement('p');
+    var myPara2 = document.createElement('p');
+
+    myH2.textContent = `${jsonObj.location.name}, ${jsonObj.location.region}`;
+    myPara1.textContent = `Courant: ${jsonObj.current.temp_c}℃, Resenti ${jsonObj.current.feelslike_c}℃`;
+    myPara2.textContent = jsonObj.current.condition.text;
+
+
+    myArticle.appendChild(myH2);
+    myArticle.appendChild(myPara1);
+    myArticle.appendChild(myPara2);
+
+    section.appendChild(myArticle);
+
+}
+
+TestMeteo()
